7ba3f39a61a02931a901fe9187f9c500c1392fee
amroabulaban <109900470+amroabulaban@users.noreply.github.com>
2024-12-04 09:51:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ app.use(cors());
 
 app.use("/", userRoute);
 
-mongoose
-  .connect(
-    "mongodb+srv://root:root@aeliasdb.t7xow.mongodb.net/?retryWrites=true&w=majority&appName=Aeliasdb"
-  )
-  .then(() => {
-    console.log("Connected to database!");
-    app.listen(8000, () => {
-      console.log("server is runnnig on port 8000....");
-    });
-  })
+// mongoose
+//   .connect(
+//     "mongodb+srv://root:root@aeliasdb.t7xow.mongodb.net/?retryWrites=true&w=majority&appName=Aeliasdb"
+//   )
+//   .then(() => {
+//     console.log("Connected to database!");
+//     app.listen(8000, () => {
+//       console.log("server is runnnig on port 8000....");
+//     });
+//   })
   .catch((error) => {
     console.log("Connection failed!", error.message);
   });
